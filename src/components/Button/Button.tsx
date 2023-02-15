4c4e8979e5e4848e341a13377ac60242e93e1c8b
Dimas Camillo <dimas.camillo@sonda.com>
2023-02-15 19:53:16 -0300

    add routers into the project

--- src/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonVariant, TagButton } from './Button.styles'
-
-interface ButtonProps {
-  variant?: ButtonVariant
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <TagButton variant={variant}>Enviar</TagButton>
-}
