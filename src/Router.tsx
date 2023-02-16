9a5a4438f23d5863a9a2ac614417c3ccef485580
Dimas Camillo <dimas.camillo@sonda.com>
2023-02-16 14:12:12 -0300

    new features

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
+import { Perfil } from "./pages/Perfil";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/login" element={<Login/>} />
+      <Route path="/perfil" element={<Perfil/>} />
     </Routes>
   );
 }
